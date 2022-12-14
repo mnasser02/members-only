e873c4cafc72d6123f7885d67e6e241e1529d0ca
Mahdi Nasser <mhnasser23@gmail.com>
2022-12-14 20:05:28 +0200

    add style

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(
   session({
     secret: process.env.SESSION_SEC,
